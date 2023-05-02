c9ee495044a9a0cd44d2da1b2556c755183892e1
cherrybleus <131594838+cherrybleus@users.noreply.github.com>
2023-05-02 09:45:36 +0800

    Update and rename GameTester.java to GameServer.java

--- GameServer.java
+++ GameServer.java
@@ -1,9 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GameTester {
-	
-	
+public class GameServer {
 
 	public static void main(String[] args) {		
 		SceneFrame bframe = new SceneFrame(1280, 620);
